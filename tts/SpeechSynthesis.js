981305eb5dabdf50d8ced7827fbb7ec1982888b9
Rasmus Blanck <rasmus.blanck@gu.se>
2022-12-08 09:47:33 +0100

    tts, mainly

---
+++ tts/SpeechSynthesis.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+
+function xmlToString(filePath) {
+    const xml = fs.readFileSync(filePath, "utf8");
+    return xml;
+}
+
+(function() {
+
+    "use strict";
+
+    var sdk = require("microsoft-cognitiveservices-speech-sdk");
+    var readline = require("readline");
+
+    var audioFile = "YourAudioFile.wav";
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SPEECH_REGION);
+    const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+
+    // The language of the voice that speaks.
+    speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural"; 
+
+    // Create the speech synthesizer.
+    var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+
+    const ssml = xmlToString("lab3A.xml");
+    synthesizer.speakSsmlAsync(
+        ssml,
+        result => {
+            if (result.errorDetails) {
+                console.error(result.errorDetails);
+            } else {
+                console.log(JSON.stringify(result));
+            }
+
+            synthesizer.close();
+        },
+        error => {
+            console.log(error);
+            synthesizer.close();
+        });
+/*
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question("Enter some text that you want to speak >\n> ", function (text) {
+      rl.close();
+      // Start the synthesizer and wait for a result.
+      synthesizer.speakTextAsync(text,
+          function (result) {
+        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+          console.log("synthesis finished.");
+        } else {
+          console.error("Speech synthesis canceled, " + result.errorDetails +
+              "\nDid you set the speech resource key and region values?");
+        }
+        synthesizer.close();
+        synthesizer = null;
+      },
+          function (err) {
+        console.trace("err - " + err);
+        synthesizer.close();
+        synthesizer = null;
+      });
+      console.log("Now synthesizing to: " + audioFile);
+    });*/
+}());
